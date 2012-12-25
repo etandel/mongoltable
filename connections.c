af8e85736fab825f4fcfdc77aea46f6a4a977cf8
Elias Tandel Barrionovo <elias.tandel@gmail.com>
2012-12-25 00:31:41 -0200

    Finally got luaL_newlib() working.

--- connections.c
+++ connections.c
@@ -17,40 +17,17 @@ static int con_unbind(lua_State *L){
     return 0;
 }
 
-/*
 static const luaL_Reg R[] = {
     {"get",	con_get},
     {"set", con_set},
     {"bind", con_bind},
-    {"unbind", con_unbind}
+    {"unbind", con_unbind},
+    {NULL, NULL},
 };
-*/
 
 LUALIB_API int luaopen_connections(lua_State *L)
 {
-/*
+    luaL_checkversion(L);
     luaL_newlib(L, R);
-*/
-
-
-    lua_createtable(L, 0, 4);
-
-    lua_pushstring(L, "get");
-    lua_pushcfunction(L, con_get);
-    lua_settable(L, 3);
-
-    lua_pushstring(L, "set");
-    lua_pushcfunction(L, con_set);
-    lua_settable(L, 3);
-
-    lua_pushstring(L, "bind");
-    lua_pushcfunction(L, con_bind);
-    lua_settable(L, 3);
-
-    lua_pushstring(L, "unbind");
-    lua_pushcfunction(L, con_unbind);
-    lua_settable(L, 3);
-
-
     return 1;
 }
