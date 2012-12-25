d562c4c99c3a2f585ed52969fa54fc20f69ca6a4
Elias Tandel Barrionovo <elias.tandel@gmail.com>
2012-12-25 01:36:51 -0200

    Add connections table.

--- connections.c
+++ connections.c
@@ -1,6 +1,9 @@
 #include "lua.h"
 #include "lauxlib.h"
 
+//key of the connections table on the registry
+#define CONNSKEY "mongoconns"
+
 static int con_get(lua_State *L){
     return 0;
 }
@@ -28,6 +31,12 @@ static const luaL_Reg R[] = {
 LUALIB_API int luaopen_connections(lua_State *L)
 {
     luaL_checkversion(L);
+
+    //create connections table
+    lua_pushstring(L, CONNSKEY);
+    lua_newtable(L);
+    lua_settable(L, LUA_REGISTRYINDEX);
+    
     luaL_newlib(L, R);
     return 1;
 }
